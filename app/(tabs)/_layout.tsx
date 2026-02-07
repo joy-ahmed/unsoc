1858c09c6c3a259f9d1ff685be72aa84063b868f
Joy Ahmed <joy.ahmed.dev@gmail.com>
2026-02-07 23:56:40 +0600

    feat: enhance tab layout with Profile and Settings screens; wrap layout in SafeAreaView

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import { Ionicons } from'@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
+import React from 'react'
 
 const TabsLayout = () => {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-            title: "Home",
-            tabBarIcon: ({color, size}) => (
-                <Ionicons name="home" size={size} color={color} />
-            )
-        }}
-      />
-    </Tabs>
-  )
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='settings'
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="settings" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
+    )
 }
 
 export default TabsLayout
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#e67272" }}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaView>
   );
 }
